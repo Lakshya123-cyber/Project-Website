46a8011541877a4a4ac33a32abe56ef52f95b08c
Lakshya Raikwal <72344300+Lakshya123-cyber@users.noreply.github.com>
2021-11-01 17:44:24 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
+// Select element function
+const selectElement = function(ele) {
+    return document.querySelector(ele);
+};
 
+let menuToggler = selectElement('.menu-toggle');
+let body = selectElement('body');
+
+menuToggler.addEventListener('click', function () {
+    body.classList.toggle('open');
+});
